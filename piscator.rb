0827a269e8c3b32371bd42e65d2b8173b28b24f9
Aaron Zemetres <azemetre@users.noreply.github.com>
2023-06-05 22:30:09 -0400

    build: version bumps (#1)

--- piscator.rb
+++ piscator.rb
@@ -1,24 +1,24 @@
 class Piscator < Formula
-  desc "My useful Go-lang command line tool"
+  desc "A tool for cloning GitHub repos"
   homepage "https://github.com/shimman-dev/piscator"
-  version "1.1.0"
+  version "1.2.0"
   license "GPLv3"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/shimman-dev/piscator/releases/download/v#{version}/piscator-v#{version}-darwin-amd64.tar.gz"
-      sha256 "bc0bdacad4957e1342c49dc98a38fbbf9fd4c1920d500b057bdb20cf94bae6e0"
+      sha256 "3148f9c19851afffbd241d7260f75aef471e9653685c57cd2176761db7512bca"
     elsif Hardware::CPU.arm?
       url "https://github.com/shimman-dev/piscator/releases/download/v#{version}/piscator-v#{version}-darwin-arm64.tar.gz"
-      sha256 "487ea16b21dce4d94122910025b1747854237dd12a26fcaaa027f66e4fb8efeb"
+      sha256 "4fd28728fe7b702caadca01ca28768cded2d74511c9147173b1b14727d48a7d6"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/shimman-dev/piscator/releases/download/v#{version}/piscator-v#{version}-linux-amd64.tar.gz"
-      sha256 "1f96684513f53acb4fe15d8ec6e30079683d6f612778864e70284070d1368545"
+      sha256 "cc155a060c4cdf306a6af8a7ebec05a67219208e80128c9fae051e8b999b39ac"
     elsif Hardware::CPU.arm?
       url "https://github.com/shimman-dev/piscator/releases/download/v#{version}/piscator-v#{version}-linux-arm64.tar.gz"
-      sha256 "1fc0094647b2b564eb4c491a375e725ec29d20e59a8431ca99f0dd956c6a90b4"
+      sha256 "85036a44f427a1b0fa477eb7d1ad34e2f42d156b137d3941b152778a9a995b6c"
     end
   end
 
